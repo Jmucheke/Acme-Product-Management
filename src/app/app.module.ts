4b717d00518bd923f1cd26d22b62f3d074ce9539
Jesse Mucheke <jmucheke274@gmail.com>
2024-02-13 23:04:53 +0300

    Worked on directives and data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './products/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from 'src/app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, ProductListComponent,FormsModule
   ],
   bootstrap: [AppComponent]
 })
